3b5686c178ad7cb3059f7318169d1af701e1563c
OLAMILEKAN <akinbiyimoses@gmail.com>
2023-12-01 10:03:10 +0100

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'> 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                A nisi amet ad assumenda qui consequatur sint fugiat recusandae fugit nesciunt nobis iste pariatur quis deserunt, 
+                illum quidem atque necessitatibus corrupti temporibus culpa. Adipisci enim illum suscipit minus doloremque autem cupiditate, 
+                praesentium pariatur eligendi debitis delectus accusantium harum, et eveniet. Soluta quia dicta exercitationem iure minima eum ut pariatur debitis quidem?
+            </p>
+            
+            <br />
+
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cupiditate ex, fugiat, dolores quas recusandae esse temporibus sequi est explicabo fuga doloremque dolore inventore, error cumque aliquam repellendus qui? Enim accusamus repellat corporis reprehenderit qui fuga! Nemo, ullam, nesciunt magni minima perspiciatis, quos consequatur quo explicabo atque nam itaque id.
+            </p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default About
